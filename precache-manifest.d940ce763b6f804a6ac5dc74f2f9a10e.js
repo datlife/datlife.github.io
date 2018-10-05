76b28b7e71ea0b8aee07da24e8d60e30d216cc93
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-10-05 07:46:10 -0700

    Updates

---
+++ precache-manifest.d940ce763b6f804a6ac5dc74f2f9a10e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6f692f236533d8c0ec2e",
+    "url": "/static/js/main.6f692f23.chunk.js"
+  },
+  {
+    "revision": "0039605f0e076ca0da7a",
+    "url": "/static/js/1.0039605f.chunk.js"
+  },
+  {
+    "revision": "6f692f236533d8c0ec2e",
+    "url": "/static/css/main.6a8ceac7.chunk.css"
+  },
+  {
+    "revision": "a87460e2298297cc0b088dcef31f1243",
+    "url": "/index.html"
+  }
+];
