b8960ef69e411e7b473d3d93bba86c953b296e6f
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-10-13 07:04:55 -0700

    Update ghost.api.jsx

--- src/api/ghost.api.jsx
+++ src/api/ghost.api.jsx
@@ -5,12 +5,13 @@ const client_id = 'ghost-frontend';
 const client_secret = 'ec2e9f4fefe0';
 
 class Ghost {
+  
   async get(path, params) {
-    const query = qs.stringify({...params, client_id,client_secret})
+    const query = qs.stringify({...params, client_id, client_secret})
     const response = await fetch(`${GHOST_PUBLIC_API}/${path}?${query}`)
-    const data = await response.json();
-    return data;
+    return await response.json();
   }
+  
   async getPage(pageNumber){
     const page = await this.get('posts',
       { page: pageNumber,
@@ -20,13 +21,9 @@ class Ghost {
         fields: "slug,id,title,feature_image,custom_excerpt,published_at"})
     return page;
   }
-  async getPost(slug){
-    const page = await this.get(`posts/slug/${slug}/`,
-      { absolute_urls: true})
-    return page;
-  }
+  
+  async getPost(slug){return await this.get(`posts/slug/${slug}/`, {absolute_urls: true})}
 }
 
 const ghostAPI = new Ghost();
-
 export default ghostAPI;
